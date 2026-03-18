5d27fc665c72b906ec9e1fc3faa78c7051ba334c
asharca <129170100+asharca@users.noreply.github.com>
2026-03-18 17:20:21 +0800

    Revert "fix(deploy): pre-bake golang-migrate into migrator image"
    This reverts commit da842851814c8e3f64e7d9c00e0d220793a3d765.

--- deploy/migrate.sh
+++ deploy/migrate.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 set -e
 
+# Install golang-migrate if not present
+if ! command -v migrate > /dev/null 2>&1; then
+    echo "Installing golang-migrate..."
+    wget -qO- https://github.com/golang-migrate/migrate/releases/download/v4.18.1/migrate.linux-amd64.tar.gz \
+        | tar xz -C /usr/local/bin migrate
+fi
+
 DB_HOST="${DB_HOST:-10.0.10.114:5432}"
 DB_USER="${DB_USER}"
 DB_PASSWORD="${DB_PASSWORD}"
